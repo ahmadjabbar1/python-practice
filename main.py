7757517830e409b0fa59290cbc670ba0fce1bb47
ahmadjabbar1 <155512481+ahmadjabbar1@users.noreply.github.com>
2024-01-10 10:59:56 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+# Write your code below this line 👇
+print("Hello World!\nHello World!")
+print('Day 1- Python Print Function')
+print("The function is declared like this:")
+print("print('what to print')")
+print("Day 1- String Manipulation")
+print("String Concatenation is done with the " + " sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+input("What is your name?")
+print("Hello" + input("what is your name?"))
+print(len("Jack"))
+name = input("What is your name?")
+name = "Jack"
+print(name)
+name = "Angela"
+print(name)
+a = input("a:")
+b = input("b:")
+c = a
+a = b
+b = c
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+#2. Ask the user for the city that they grew up in.
+city = input("What's name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What's your pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#Data Types
+#String
+print("Hello"[4])
+print("123" + "345")
+#Integer
+print(123 + 345)
+123_456_789
+#Float
+3.14159
+#Boolean
+True
+False
+#num_char = len(input("What is your name?"))
+#new_num_char = str(num_char)
+#print("Your name has " + new_num_char + " characters.")
+a = float(123)
+print(type(a))
+print(70 + float("100.5"))
+print(str(70) + str(100))
